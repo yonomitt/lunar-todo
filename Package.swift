3e81cf131afa4f8241ea326699e22ffe1ef83756
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-16 21:08:57 +0200

    Added dependencies on Titan by Bermuda Digital Studio

--- Package.swift
+++ Package.swift
@@ -3,5 +3,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "todo-moonshot"
+    name: "todo-moonshot",
+    dependencies: [
+        .Package(url: "https://github.com/bermudadigitalstudio/Titan.git", Version("0.7.2")),
+        .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0, minor: 5)
+    ]
 )
