2e21dc86ad4bef40ea3b869ad5349d53eb7ec666
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-16 21:59:11 +0200

    Test using a simple example from the Titan README.md

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Titan
+import TitanKituraAdapter
+
+let app = Titan()
+
+/// Hello World, req is sent to next matching route 
+app.get("/") { req, _ in
+    return (req, Response(200, "Hello World"))
+}
+
+// start the Kitura webserver on port 8088
+TitanKituraAdapter.serve(app.app, on: 8088)
+
