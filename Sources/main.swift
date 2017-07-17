d0c811a99458de7283d40c1b221388cb25642594
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-17 15:46:44 +0200

    Read in the necessary environment variables

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,6 +2,9 @@ import Foundation
 import Titan
 import TitanKituraAdapter
 
+let env = ProcessInfo.processInfo.environment
+let URLBASE = env["SERVER_HOST"] ?? ""
+
 let todoController = TodoController(app: Titan())
 
 // start the Kitura webserver on port 8088
