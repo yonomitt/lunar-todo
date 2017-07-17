31e2bd951125bc7b720033ef33e31fe1c982d20d
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-17 16:31:04 +0200

    Added ability to change the order field of a todo item in the todo list

--- Sources/TodoList.swift
+++ Sources/TodoList.swift
@@ -65,6 +65,17 @@ struct TodoList {
         return nil
     }
     
+    mutating func updateOrder(for id: Int, order: Int) -> TodoItem? {
+        
+        if let index = items.index(where: { $0.id == id }) {
+            items[index].order = order
+            
+            return items[index]
+        }
+        
+        return nil
+    }
+    
     func jsonArray() -> [JSONDict] {
         return items.map { $0.jsonDict() }
     }
