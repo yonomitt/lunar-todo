070a7cd4b517a1a862fb0ebd280819c5dfac3c4d
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-17 22:00:06 +0200

    Add Rope Postgres driver by Bermuda Digital Studio as a dependency

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "todo-moonshot",
     dependencies: [
         .Package(url: "https://github.com/bermudadigitalstudio/Titan.git", Version("0.7.2")),
-        .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/bermudadigitalstudio/Rope.git", Version("0.5.1"))
     ]
 )
