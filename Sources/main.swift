f18a6b5ed74bd1fb287378fdf32587471efb8260
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-17 22:07:21 +0200

    Import Rope, read the database credentials from the environment, and attempt to connect to the database

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,19 @@
 import Foundation
+import Rope
 import Titan
 import TitanKituraAdapter
 
+
 let env = ProcessInfo.processInfo.environment
 let URLBASE = env["SERVER_HOST"] ?? ""
+
+let dbHost = env["POSTGRES_HOST"] ?? ""
+let dbUser = env["POSTGRES_USER"] ?? ""
+let dbPassword = env["POSTGRES_PASSWORD"] ?? ""
+let dbName = env["POSTGRES_DB"] ?? ""
+
+let dbCredentials = RopeCredentials(host: dbHost, port: 5432,  dbName: dbName, user: dbUser, password: dbPassword)
+let db = try? Rope.connect(credentials: dbCredentials)
 
 let todoController = TodoController(app: Titan())
 
