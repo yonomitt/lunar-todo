3ef0dfe810438a9b10c334d58deeb906ad5c0cab
Yonatan Mittlefehldt <yono@toojuice.com>
2017-07-17 21:20:10 +0200

    Renamed functions to clarify their purpose and cleaned up comments to show the associated HTTP command

--- Sources/TodoController.swift
+++ Sources/TodoController.swift
@@ -13,50 +13,46 @@ final class TodoController {
     init(app: Titan) {
         self.app = app
         
-        // GET all todo items
-        self.app.get("/", handleWebroot)
+        self.app.get("/", getAllTodoItems)
         
-        // GET a single todo item
-        self.app.get("/*", handleGetItem)
+        self.app.get("/*", getOneTodoItem)
         
-        // POST a todo item
-        self.app.post("/", handlePostTodo)
+        self.app.post("/", createTodoItem)
         
-        // PATCH a todo item
-        self.app.patch("/*", handlePatchTodo)
+        self.app.patch("/*", editTodoItem)
         
-        // DELETE all todo items
-        self.app.delete("/", handleDelete)
+        self.app.delete("/", deleteAllTodoItems)
         
-        // DELETE a single todo item
-        self.app.delete("/*", handleDeleteItem)
+        self.app.delete("/*", deleteOneTodoItem)
         
         addInsecureCORSSupport(self.app)
     }
-    
-    /// This function will handle the GET requests at the root page
-    
-    func handleWebroot(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
+}
+
+// MARK: Handler functions
+
+extension TodoController {
+
+    /// GET request at the API root to retrieve all todo items
+    func getAllTodoItems(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
         
         return (req, Response(200, String.from(todoList.jsonArray())))
     }
     
-    /// This function will handle GET requests for a particular todo url
-    
-    func handleGetItem(req: RequestType, id: String, res: ResponseType) -> (RequestType, ResponseType) {
+    /// GET request for single todo item based on ID string
+    func getOneTodoItem(req: RequestType, id: String, res: ResponseType) -> (RequestType, ResponseType) {
         
         if let id = Int(id),
             let item = todoList.findItem(for: id) {
             
             return (req, Response(200, String.from(item.jsonDict())))
         }
-
+        
         return (req, Response(400))
     }
     
-    /// This function handles the todo POST requests
-    
-    func handlePostTodo(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
+    /// POST request to create a single todo item
+    func createTodoItem(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
         
         guard let json = req.json as? JSONDict else {
             return (req, Response(400))
@@ -71,9 +67,8 @@ final class TodoController {
         return (req, Response(200, String.from(todoItem.jsonDict())))
     }
     
-    /// This function handles the todo PATCH requests
-    
-    func handlePatchTodo(req: RequestType, id: String, res: ResponseType) -> (RequestType, ResponseType) {
+    /// PATCH request to edit todo items -- title, completed, and order can be changed
+    func editTodoItem(req: RequestType, id: String, res: ResponseType) -> (RequestType, ResponseType) {
         
         if let json = req.json as? JSONDict,
             let id = Int(id) {
@@ -100,18 +95,16 @@ final class TodoController {
         return (req, Response(400))
     }
     
-    /// This function handles the todo DELETE requests
-    
-    func handleDelete(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
+    /// DELETE request at the API root to delete all todo items
+    func deleteAllTodoItems(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
         
         todoList.clear()
         
         return (req, Response(200))
     }
     
-    /// This function will handle GET requests for a particular todo url
-    
-    func handleDeleteItem(req: RequestType, id: String, res: ResponseType) -> (RequestType, ResponseType) {
+    /// DELETE request for single todo item based on ID string
+    func deleteOneTodoItem(req: RequestType, id: String, res: ResponseType) -> (RequestType, ResponseType) {
         
         if let id = Int(id) {
             todoList.delete(for: id)
